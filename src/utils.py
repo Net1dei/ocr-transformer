4f1a86a900f14e090c1ea2db4a66c1eaa2465271
CHex0K <116585490+CHex0K@users.noreply.github.com>
2023-07-16 17:26:48 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -217,7 +217,7 @@ def evaluate(model, criterion, loader, case=True, punct=True):
             metrics['wer'] += sum([int(true_phrases[i] != pred_phrases[i]) \
                         for i in range(BATCH_SIZE)])/BATCH_SIZE
         
-            print(metrics['wer'])
+            print('TEST',metrics['wer'])
 
             for i in range(len(true_phrases)):
               result['true'].append(true_phrases[i])
