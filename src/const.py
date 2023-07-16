86b27b0e6b77d4651394b815b3320ed62ea6fa6f
CHex0K <116585490+CHex0K@users.noreply.github.com>
2023-07-16 18:21:04 +0300

    Update const.py

--- src/const.py
+++ src/const.py
@@ -1,14 +1,14 @@
 from pathlib import Path
-import os
+
 
 DIR = Path.cwd() # work directory
-PATH_TEST_DIR = os.fspath(Path(DIR, 'test'))
-PATH_TEST_LABELS = os.fspath(Path( DIR, 'test.tsv'))
-PATH_TRAIN_DIR = os.fspath(Path( DIR, 'train'))
-PATH_TRAIN_LABELS = os.fspath(Path( DIR, 'train.tsv'))
-PREDICT_PATH = os.fspath(Path(DIR, 'test'))
-CHECKPOINTS_PATH = os.fspath(Path(DIR))
-FROM_CHECKPOINT_PATH = '/content/drive/MyDrive/ocr_transformer_4h2l_simple_conv_64x256.pt' # if not None then training start with this checkpoint
-WEIGHTS_PATH = '/content/drive/MyDrive/ocr_transformer_4h2l_simple_conv_64x256.pt'
-PATH_TEST_RESULTS = os.fspath(Path(DIR, 'test_rn50_4h2l_result.tsv'))
-TRAIN_LOG = os.fspath(Path(DIR, 'train_log.tsv'))
+PATH_TEST_DIR = Path(DIR, 'test')
+PATH_TEST_LABELS = Path( DIR, 'test.tsv')
+PATH_TRAIN_DIR = Path( DIR, 'train')
+PATH_TRAIN_LABELS = Path( DIR, 'train.tsv')
+PREDICT_PATH = Path(DIR, 'test')
+CHECKPOINTS_PATH = Path(DIR)
+FROM_CHECKPOINT_PATH = Path(DIR, 'drive/MyDrive/ocr_transformer_4h2l_simple_conv_64x256.pt') # if not None then training start with this checkpoint
+WEIGHTS_PATH = Path(DIR, 'drive/MyDrive/ocr_transformer_4h2l_simple_conv_64x256.pt')
+PATH_TEST_RESULTS = Path(DIR, 'test_rn50_4h2l_result.tsv')
+TRAIN_LOG = Path(DIR, 'train_log.tsv')
