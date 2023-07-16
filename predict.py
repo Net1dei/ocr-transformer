11fb57a889938403b0d752fdc5336e183344663a
CHex0K <116585490+CHex0K@users.noreply.github.com>
2023-07-16 19:53:59 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ import sys
 import torch
 import random
 import pathlib
+import os
 sys.path.append(str(pathlib.Path(__file__).parent.resolve())+'/src')
 
 from const import DIR, PATH_TEST_DIR, PATH_TEST_LABELS, WEIGHTS_PATH, PREDICT_PATH
