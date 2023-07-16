5008bd5ddb71a3b5236361eb3a5efaaa437ae7ed
CHex0K <116585490+CHex0K@users.noreply.github.com>
2023-07-16 18:10:13 +0300

    Update const.py

--- src/const.py
+++ src/const.py
@@ -3,11 +3,11 @@ import os
 
 DIR = Path.cwd() # work directory
 PATH_TEST_DIR = os.fspath(Path(DIR, 'test'))
-PATH_TEST_LABELS = os.fspath((Path( DIR, 'test.tsv'))
-PATH_TRAIN_DIR = os.fspath((Path( DIR, 'train'))
+PATH_TEST_LABELS = os.fspath(Path( DIR, 'test.tsv'))
+PATH_TRAIN_DIR = os.fspath(Path( DIR, 'train'))
 PATH_TRAIN_LABELS = os.fspath(Path( DIR, 'train.tsv'))
 PREDICT_PATH = os.fspath(Path(DIR, 'test'))
-CHECKPOINTS_PATH = os.fspath((Path(DIR))
+CHECKPOINTS_PATH = os.fspath(Path(DIR))
 FROM_CHECKPOINT_PATH = '/content/drive/MyDrive/ocr_transformer_4h2l_simple_conv_64x256.pt' # if not None then training start with this checkpoint
 WEIGHTS_PATH = '/content/drive/MyDrive/ocr_transformer_4h2l_simple_conv_64x256.pt'
 PATH_TEST_RESULTS = os.fspath(Path(DIR, 'test_rn50_4h2l_result.tsv'))
