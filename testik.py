915dbaa1ecef57c371ae6295c0aa77170db527b6
CHex0K <116585490+CHex0K@users.noreply.github.com>
2023-07-17 09:26:39 +0300

    Update testik.py

--- testik.py
+++ testik.py
@@ -1,1 +1,11 @@
+import sys
+import torch
+import random
+import pathlib
+import os
+sys.path.append(str(pathlib.Path(__file__).parent.resolve())+'/src')
 
+from const import DIR, PATH_TEST_DIR, PATH_TEST_LABELS, WEIGHTS_PATH, PREDICT_PATH
+
+def aboba():
+  print(DIR)
